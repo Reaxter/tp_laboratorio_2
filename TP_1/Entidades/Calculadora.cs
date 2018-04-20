517319b5d57c3c1f5269cb6f7233df240eb019fd
Reaxter <ajlm_97@hotmail.com>
2018-04-20 17:17:13 -0300

    Trabajo Practico 1

---
+++ TP_1/Entidades/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double numero = 0;
+            switch (ValidarOperador(operador))
+            {
+                case "-":
+                    numero = num1 - num2;
+                    break;
+                case "*":
+                    numero = num1 * num2;
+                    break;
+                case "/":
+                    numero = num1 / num2;
+                    break;
+                case "+":
+                    numero = num1 + num2;
+                    break;
+            }
+            return numero;
+        }
+        private static string ValidarOperador(string operador)
+        {
+            if (operador == null)
+                return "+";
+            if (operador != "+" && operador != "-" && operador != "/" && operador != "*")
+                return "+";
+            return operador;
+        }
+    }
+}
---
+++ TP_1/Entidades/Numero.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero = 0;
+
+        private string SetNumero
+        {
+            set { numero = ValidarNumero(value); }
+        }
+        public static string BinarioDecimal(string binario)
+        {
+            if (string.IsNullOrEmpty(binario))
+                return "Valor Invalido";
+            for (int i = 0; i < binario.Count(); i++)
+            {
+                if (binario[i] != '0' && binario[i] != '1')
+                    return "Valor Invalido";
+            }
+            return Convert.ToInt64(binario, 2).ToString();
+        }
+        public static string DecimalBinario(double numero)
+        {
+            if (numero == double.NaN)
+                return "Valor Invalido";
+            return DecimalBinario(Convert.ToString(numero));
+        }
+        public static string DecimalBinario(string numero)
+        {
+            if (string.IsNullOrEmpty(numero) || numero.Count() > 16)
+                return "Valor Invalido";
+            for (int i = 0; i < numero.Count(); i++)
+            {
+                if (!(char.IsDigit(numero[i])))
+                    return "Valor Invalido";
+            }
+            return Convert.ToString(Int64.Parse(numero), 2).ToString();
+        }
+        public Numero() : this(0)
+        {
+        }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        public Numero(string strNumero)
+        {
+            SetNumero = strNumero;
+        }
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        public static double operator /(Numero n1, Numero n2)
+        {
+            return n1.numero / n2.numero;
+        }
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        private double ValidarNumero(string strNumero)
+        {
+            double numero;
+            if (string.IsNullOrEmpty(strNumero) || !(double.TryParse(strNumero, out numero)))
+                return 0;
+            else
+                return numero;
+        }
+    }
+}
