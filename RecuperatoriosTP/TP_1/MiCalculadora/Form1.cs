d8734468c617293df2ac32dd26d27ca26fe0aa7d
Reaxter <ajlm_97@hotmail.com>
2018-07-01 12:16:46 -0300

    RecuperatoriosTP

---
+++ RecuperatoriosTP/TP_1/MiCalculadora/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class LaCalculadora : Form
+    {
+        public LaCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = LaCalculadora.Operar(this.txtNumero1.Text, this.txtNumero2.Text, this.cmbOperador.Text).ToString();
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            return Calculadora.Operar(new Numero(numero1), new Numero(numero2), operador);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtNumero1.Text = "";
+            this.txtNumero2.Text = "";
+            this.lblResultado.Text = "";
+            this.cmbOperador.Text = "";
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Numero.DecimalBinario(this.lblResultado.Text);
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Numero.BinarioDecimal(this.lblResultado.Text);
+        }
+    }
+}
